c186403a4352cf3e0970904bb3de168b8978fb4d
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2023-12-06 15:49:00 +0100

    Add files via upload

--- index002/Cours003.sql
+++ index002/Cours003.sql
@@ -1,4 +1,38 @@
 /*
 Cours : https://selectstarsql.com/longtail.html
-Date : 
+Date : 06-12-2023
 */
+
+/*
+SELECT * FROM executions
+*/
+
+-- Dresser la liste de tous les comtés distincts de l'ensemble de données
+-- SELECT
+--     County
+-- FROM executions
+-- GROUP BY County
+
+-- Cette requête permet de connaître le nombre d'exécutions par comté
+-- SELECT
+--   County,
+--   COUNT(*) AS county_executions
+-- FROM executions
+-- GROUP BY County
+
+-- Comptez le nombre de détenus âgés de 50 ans ou plus 
+-- qui ont été exécutés dans chaque comté.
+-- SELECT 
+--     County, COUNT(*)
+-- FROM executions
+-- WHERE AgeatExecution >= 50
+-- GROUP BY County
+
+-- Liste des comtés dans lesquels plus de 2 détenus âgés de 50 ans 
+-- ou plus ont été exécutés
+-- SELECT
+--     County, COUNT(*)
+-- FROM executions
+-- WHERE AgeatExecution >= 50
+-- GROUP BY County
+-- HAVING COUNT(*) > 2
