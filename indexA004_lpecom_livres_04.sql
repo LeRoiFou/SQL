4717a2617335a052bae9fbf05c8aaa4798b9599a
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-05 10:42:43 +0200

    Add files via upload

---
+++ indexA004_lpecom_livres_04.sql
@@ -0,0 +1,8 @@
+/*
+Lien : https://aymeric-auberton.fr/academie/mysql/exercices
+
+Exercice 4
+*/
+
+-- Quelle requête utiliser pour récupérer le prix du livre le plus élevé de la table lpecom_livres ?
+SELECT MAX(prix) FROM lpecom_livres
---
+++ indexA007_lpecom_livres_07.sql
@@ -0,0 +1,8 @@
+/*
+Lien : https://aymeric-auberton.fr/academie/mysql/exercices
+
+Exercice 7
+*/
+
+-- Quelle requête utiliser pour récupérer le prix du livre le moins élevé de la table lpecom_livres 
+-- en renommant la colonne dans les résultats par minus ?
