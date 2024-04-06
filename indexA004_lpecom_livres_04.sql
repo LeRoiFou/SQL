393e78a94d29cd82d93e8549b0a0967730e8174f
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-06 10:02:50 +0200

    Delete indexA004_lpecom_livres_04.sql

--- indexA004_lpecom_livres_04.sql
+++
@@ -1,8 +0,0 @@
-/*
-Lien : https://aymeric-auberton.fr/academie/mysql/exercices
-
-Exercice 4
-*/
-
--- Quelle requête utiliser pour récupérer le prix du livre le plus élevé de la table lpecom_livres ?
-SELECT MAX(prix) FROM lpecom_livres
