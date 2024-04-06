4e3b26a4beafc4f6aeaf76bac551bfe0a8ec4c2d
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-06 10:02:23 +0200

    Delete indexA007_lpecom_livres_07.sql

--- indexA007_lpecom_livres_07.sql
+++
@@ -1,8 +0,0 @@
-/*
-Lien : https://aymeric-auberton.fr/academie/mysql/exercices
-
-Exercice 7
-*/
-
--- Quelle requête utiliser pour récupérer le prix du livre le moins élevé de la table lpecom_livres 
--- en renommant la colonne dans les résultats par minus ?
