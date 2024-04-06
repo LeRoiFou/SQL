dbd2abd3ff0a4fc40aa61489fb08e78115ae5005
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-06 10:03:15 +0200

    Delete indexA001_lpecom_livres_01.sql

--- indexA001_lpecom_livres_01.sql
+++
@@ -1,9 +0,0 @@
-/*
-Lien : https://aymeric-auberton.fr/academie/mysql/exercices
-
-Exercice 1
-*/
-
--- Quelle requête utiliser pour afficher l'ensemble des enregistrements de la table lpecom_livres ?
-SELECT * FROM lpecom_livres;
-
