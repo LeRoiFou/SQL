1dcf40b0c4bb50208d5a3c3b3d319bd77973d0a9
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-19 06:01:29 +0200

    Add files via upload

---
+++ indexA039_Partie05_Exo05.sql
@@ -0,0 +1,25 @@
+/*
+Lien : https://aymeric-auberton.fr/academie/mysql/exercices
+
+Sujet d'actualité des années 2020 et 2021, la pandémie du COVID-19 va être au coeur de notre sujet pour cette nouvelle partie d'exercices.
+
+La table lpecom_covid liste le nombre quotidien de personnes ayant reçu au moins une dose, par date d'injection, par région. 
+Les colonnes n_cum_dose1 et n_cum_dose2 s'occupent de cumuler le nombre d'injection. 
+Les colonnes couv_dose1 et couv_dose2 calculent la couverture vaccinale des régions chaque jour.
+
+Dans les tableaux, seul un extrait des données est affiché, donc toutes les lignes des tables ne sont pas listées. 
+
+Exercice 05 - Partie V
+Tables utilisées :
+lpecom_covid
+lpecom_regions
+*/
+
+-- Quelle requête SQL utiliser pour afficher le nombre au cumulé de vaccination première dose 
+-- pour la région avec le code 93 uniquement pour le mois de mars 2021 
+SELECT 
+    SUM(n_dose2)
+FROM
+    lpecom_covid
+WHERE
+    MONTH(jour) = 3 AND id_region = 11
